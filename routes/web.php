b114bd590de7bbe53267a2cf09fd5e67a181a3c2
stel <mirano.christel28@gmail.com>
2026-06-10 23:15:45 +0800

    feat: add student module with migration, model, controller, and views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::resource('students', StudentController::class);
